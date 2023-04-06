6e03806f36be5b6960c4517bff923712db1d4894
Michal <mstankiewicz2000@gmail.com>
2023-04-06 19:27:49 +0200

    moved source files inside project

--- nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/xsim_webtalk.tcl
+++ nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Apr  5 21:23:06 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Apr  6 19:25:48 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "9fa83574-8bfe-4db4-aa8c-ac730af54112" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "a80c4fbcbe7d491e84f4ca17c8196fc5" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "70" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "74" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8750H CPU @ 2.20GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "7420_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 449016841 -regid "" -xml D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 2091294209 -regid "" -xml D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/fpga_projects/SDUP/projekt/nlms_accelerator_ip/nlms_accelerator_ip.sim/sim_1/behav/xsim/xsim.dir/nlms_x_fifo__TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
